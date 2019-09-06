fe88c85d405cd02307c16eadfc6d54fba3c02273
Jamal Frank <jamaljfrank@gmail.com>
2019-09-06 04:32:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def long_planeteer_calls(array)
     puts false 
   else true 
 end
+end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
