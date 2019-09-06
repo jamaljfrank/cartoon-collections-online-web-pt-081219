6dc14cf3a9cfdd25b26435b80996cae37142cb38
Jamal Frank <jamaljfrank@gmail.com>
2019-09-06 04:12:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.titlize.map {|name| puts "#{name}!" }
+  array.titleize.map {|name| puts "#{name}!" }
 end
 
 def long_planeteer_calls# code an argument here
